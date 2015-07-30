8765e0f9fc3ae2e522bcbc2d0d499f441544b087
liguobin82 <hbliyu2001@126.com>
2015-07-30 16:20:03 +0800

    Date：2015-07-30 Author：liguobin

--- src/com/test/MposPerformance.java
+++ src/com/test/MposPerformance.java
@@ -1,10 +1,19 @@
 package com.test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MposPerformance {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*
+	 * @Description:这个脚本是性能测试主题脚本，到时候直接放到Loadrunner上去执行。
+	 * @可能对集合点、事物开始和结束定义等，根据实际情况进行语句删减
+	 */
+	
+	public static void main(String[] args) throws IOException {
 		JsonMessage json = new JsonMessage();
 		String jsonmessage="{\"amount\" : \"000000000100\","
 				+ "\"baseStation\" : \"0000262302b9bf02\","
@@ -27,6 +36,67 @@ public class MposPerformance {
 				+ "\"track3Data\" : \"3933303437373634303130313838323031343338363D30393930303030303030373530313038363033303536333639303130303030303230303730313030303030303030303030303030303030303030303030303030414642314543374236393036443935333031\","
 				+ "\"transToken\" : \"2015072900000074\","
 				+ "\"mac\" : \"5A6964562009D362\"}";
+		String Host="192.168.7.147";
+		String urlStr = "http://" + Host + "/xposp/online/v2/transfers/consume/";
+		int code = -1;
+	    HttpURLConnection urlConn = null;
+	    OutputStream outStrm = null;
+	    try {
+		    System.out.println("transaction1");
+
+		    URL url = new URL(urlStr);
+			
+		    urlConn = (HttpURLConnection) url.openConnection();
+		    urlConn.setDoOutput(true); //post 
+		    urlConn.setUseCaches(false);//caches
+		    urlConn.setRequestMethod("POST");
+		    urlConn.setRequestProperty("Encoding","UTF-8");
+		    urlConn.setRequestProperty("Http-Method","POST");
+		    urlConn.setRequestProperty("Content-Type",";charset=UTF-8");
+		    urlConn.setRequestProperty("connection","Keep-Alive");
+		    urlConn.setRequestProperty("Content-Length","654");
+		    urlConn.setRequestProperty("Cookie","JSESSIONID=f4nje6m695b6");
+		    urlConn.setRequestProperty("Cookie2","$Version=1");
+		    urlConn.setRequestProperty("Expect","100-continue");
+		    urlConn.setRequestProperty("Host",Host);
+		    urlConn.setRequestProperty("X-Xposp-AccessToken","d6f48588-26d9-4f17-820a-f6e5fd5d9f92");
+		    urlConn.setRequestProperty("X-Xposp-Date","2015-07-30 22:18:18");
+		    
+		    outStrm = urlConn.getOutputStream();
+		    String content = "consumeReqPayload="+json.JsonMessageCode(jsonmessage);
+		    outStrm.write(content.getBytes("UTF-8"));
+		    outStrm.flush();
+		    //outStrm.close();
+		    
+		    System.out.println(urlConn.getResponseCode());
+		    
+		    if(urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			System.out.println("success");
+		    } else {
+			System.out.println("fail");
+		    }
+		//Thread.sleep(500);//1000 500 250 
+		//lr.end_transaction("transaction1", lr.AUTO );
+		    System.out.println("transaction1 failed");
+		code = urlConn.getResponseCode();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			//lr.error_message(e.getMessage());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			//lr.error_message(e.getMessage());
+			//return -1;
+		}finally{
+		     if(urlConn!=null){
+			 urlConn.disconnect();
+		     }
+		     if(outStrm!=null){
+			 outStrm.close();
+		     }
+		}
+		
+		//return code;
 		
 		System.out.println("转码前报文串为："+jsonmessage);
 		
